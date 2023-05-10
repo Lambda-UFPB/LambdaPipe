b9e9e297e1acd5de58a9bf1ca698bacb26c9e7cd
kdunorat <kdu.norat@gmail.com>
2023-05-10 07:35:40 -0300

    Fixing prototype

--- main.py
+++ main.py
@@ -9,17 +9,22 @@ options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=options)
 driver.get("https://pharmit.csb.pitt.edu/search.html")
 driver.implicitly_wait(0.5)
-driver.maximize_window()
 
-# Uploading Session
-upload_session = driver.find_element(By.XPATH, '//*[@id="pharmit"]/div[2]/div[5]/div/div/input')
-upload_session.send_keys("/home/kdunorat/Documentos/LambdaPipe/files/pharmit.json")
+# Upload Ligand
+ligand = driver.find_element(By.XPATH, '//*[@id="pharmit"]/div[2]/div[3]/div[3]/div[2]/input')
+ligand.send_keys("/home/kdunorat/Documentos/LambdaPipe/files/7KR1-pocket3-remdesivir-cid76325302.pdbqt")
+
+# Upload Receptor
+time.sleep(3)
+receptor = driver.find_element(By.XPATH, '//*[@id="pharmit"]/div[2]/div[3]/div[3]/div[1]/input')
+receptor.send_keys("/home/kdunorat/Documentos/LambdaPipe/files/7KR1.pdbqt")
 
 # Modificar o value dps
 # Searching
 search = driver.find_element(By.XPATH, '//*[@id="pharmitsearchbutton"]')
 search.click()
+
 # Saving
 time.sleep(5)
-save = driver.find_element(By.XPATH, '//*[@id="pharmit"]/div[1]/div[4]/div[3]/div/button[2]')
-save.click()
+driver.find_element(By.XPATH, '//*[@id="pharmit"]/div[1]/div[4]/div[3]/div/button[2]').click()
+
