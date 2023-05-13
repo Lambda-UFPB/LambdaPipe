7eccb7af5860ff3c2f5391c94ddd3a73a206450a
kdunorat <kdu.norat@gmail.com>
2023-05-12 21:17:11 -0300

    script for 1 db working

--- json_handler.py
+++ json_handler.py
@@ -4,14 +4,34 @@ import os
 
 class JsonHandler:
     def __init__(self):
+        self.session = self._load_json()
+
+    @staticmethod
+    def _get_path():
         cmd_1 = "xdg-user-dir DOWNLOAD"
         download_path = os.popen(cmd_1).read()
         download_path = download_path.replace("\n", "")
+        download_path = download_path + "/pharmit.json"
         new_path = f"{os.getcwd()}/files"
-        cmd_2 = f"mv {download_path}/pharmit.json {new_path}"
-        os.system(cmd_2)
-        with open(f"{new_path}/pharmit.json", 'r') as file:
-            self.session = json.load(file)
+        return download_path, new_path
+
+    @staticmethod
+    def _load_json():
+        download_path, new_path = JsonHandler._get_path()
+        cmd_2 = f"mv {download_path} {new_path}"
+        while True:
+            if os.path.exists(download_path):
+                os.system(cmd_2)
+                with open(f"{new_path}/pharmit.json", 'r') as file:
+                    session = json.load(file)
+                    break
+        return session
+
+    @staticmethod
+    def check_existence():
+        download_path, _ = JsonHandler._get_path()
+        if os.path.exists(download_path):
+            os.remove(download_path)
 
     def _pharma_switch(self):
         for i in range(4, 20):
--- pharmit_control.py
+++ pharmit_control.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from json_handler import JsonHandler
 import time
 
 
@@ -17,7 +18,7 @@ class PharmitControl:
         # Get page
         self.driver.get("https://pharmit.csb.pitt.edu/search.html")
         self.driver.implicitly_wait(3)
-        """
+
         # Upload ligand
         ligand = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[3]/div[3]/div[2]/input')
         ligand.send_keys("/home/kdunorat/Documentos/LambdaPipe/files/7KR1-pocket3-remdesivir-cid76325302.pdbqt")
@@ -25,14 +26,19 @@ class PharmitControl:
         time.sleep(3)
         receptor = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[3]/div[3]/div[1]/input')
         receptor.send_keys("/home/kdunorat/Documentos/LambdaPipe/files/7KR1.pdbqt")
-        """
+
     def _get_json(self):
         # Download first json
+        JsonHandler.check_existence()
         time.sleep(3)
         self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[5]/div/button').click()
 
     def _upload_json(self):
-        # Upload modified json
+        # Create second json
+        jsh = JsonHandler()
+        jsh.create_json()
+
+        # Upload session
         load_session = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[5]/div/div/input')
         load_session.send_keys("/home/kdunorat/Documentos/LambdaPipe/files/file_1.json")
 
@@ -42,35 +48,30 @@ class PharmitControl:
         search = self.driver.find_element(By.XPATH, '//*[@id="pharmitsearchbutton"]')
         time.sleep(2)
         search.click()
-        start_time = time.time()
-        total_time = 0
-        hits = 0
-        while total_time < 25:
-            current_time = time.time()
-            total_time = current_time - start_time
+        proceed = False
+
+        while True:
+            minimize_button = self.driver.find_element(By.XPATH, '//*[@id="pharmit"]/div[1]/div[4]/div[3]/div/button[1]')
             try:
                 no_results = self.driver.find_element(By.CLASS_NAME, "dataTables_empty")
                 if no_results.text == 'No results found':
                     print(no_results.text)
                     break
-                else:
-                    end_search = self.driver.find_element(By.XPATH,
-                                                          "/html/body/div[1]/div[1]/div[4]/div[2]/div/div[1]/span")
-                    hits = end_search.text
-                    hits = hits.split(' ')
-                    hits = hits[-2]
-                    if "," in hits:
-                        hits = hits.replace(",", "")
-                    hits = int(hits)
-                    break
-
             except NoSuchElementException:
-                time.sleep(1)
+                pass
 
-    def _download(self):
+            if minimize_button.is_enabled():
+                proceed = True
+                break
+            else:
+                time.sleep(1)
+        if proceed:
+            self._download(minimize_button)
+
+    def _download(self, minimize_button):
         # Minimize
         time.sleep(3)
-        self.driver.find_element(By.XPATH, '//*[@id="pharmit"]/div[1]/div[4]/div[3]/div/button[1]').click()
+        minimize_button.click()
         try:
             self.driver.switch_to.alert.dismiss()
         except NoAlertPresentException:
@@ -86,7 +87,6 @@ class PharmitControl:
 
     def run(self):
         self._upload_files()
-        # self._get_json()
+        self._get_json()
         self._upload_json()
         self._search()
-        self._download()
