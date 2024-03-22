764797bce75293b736694636ec997572034133ef
kdunorat <kdu.norat@gmail.com>
2024-03-22 00:55:14 -0300

    melhorando o html final

--- lambdapipe/get_html.py
+++ lambdapipe/get_html.py
@@ -18,7 +18,10 @@ def get_df_html(df_html_old: pd.DataFrame):
     df_html['Score Pharmit'] = df_html_old['Score Pharmit']
     df_html['RMSD Pharmit'] = df_html_old['RMSD Pharmit']
     df_html = df_html.sort_values(by='score', ascending=True)
+    df_html = df_html.reset_index(drop=True)
     df_html = df_html.drop(columns=['mean_toxicity', 'std_toxicity', 'score'])
+    if len(df_html) > 500:
+        df_html = df_html.head(500)
     return df_html
 
 
@@ -27,7 +30,7 @@ def results_to_html(output_folder_path: str, folder_name: str):
     df = pd.read_csv(f"{output_folder_path}/results/admet_filtered.csv")
     df_html = get_df_html(df)
     styled_html_df = df_html.style.map(color_format, subset=df_html.columns[1:12])
-    html = styled_html_df.to_html()
+    html = styled_html_df.to_html(index=False)
     with open(f"{output_folder_path}/results/{folder_name}_results.html", "w") as f:
         f.write(html)
 
